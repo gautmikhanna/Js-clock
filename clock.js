c91f6c812dc2f3572aaf3bcf33e694b24ca50fb5
GAUTMI KHANNA <gautmi.khanna@gmail.com>
2022-01-20 21:23:25 +0530

    initial commit

---
+++ clock.js
@@ -0,0 +1,19 @@
+const secondHand= document.querySelector('.second-hand');
+const minsHand = document.querySelector('.mins-hand');
+const hoursHand= document.querySelector('.hour-hand');
+function setDate(){
+    const now= new Date();
+const seconds = now.getSeconds();
+const secondsDegrees =((seconds/60)*360)+90;
+secondHand.style.transform =`rotate(${secondsDegrees}deg)`;
+
+const mins = now.getMinutes();
+const minsDegrees =((mins/60)*360)+90;
+minsHand.style.transform=`rotate(${minsDegrees}deg)`;
+
+const hours = now.getHours();
+const hourDegrees =((mins/12)*360)+90;
+hoursHand.style.transform=`rotate(${hourDegrees}deg)`;
+
+}
+setInterval(setDate,1000);
